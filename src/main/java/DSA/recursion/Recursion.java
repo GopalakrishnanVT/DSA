da7e11387ad33cea4282b83098f4fbbbca2fee31
Gopalakrishnan <Gopalakrishnanvt91@gmail.com>
2024-11-27 09:18:31 +0530

    Recursion

--- src/main/java/DSA/recursion/Recursion.java
+++ src/main/java/DSA/recursion/Recursion.java
@@ -1,4 +1,23 @@
+
 package DSA.recursion;
 
+import java.util.Scanner;
+
 public class Recursion {
+    static int sumArray(int n){
+        int[] arr={1,2,4,7,8,9};
+        if(n==0){
+            return arr[n];
+        }
+
+        int sum=arr[n]+sumArray(n-1);
+
+        return sum;
+    }
+    public static void main(String[] args) {
+        int n=5;
+
+      System.out.println(sumArray(n));
+
+    }
 }
